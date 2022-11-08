c006f7c62b5e605ba01247dc9b7168ad7bfa09d9
Sharmin Moon <sharminmoon0308@gmail.com>
2022-11-08 10:58:57 +0600

    cors and express middleware added

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+
+const { connectDB } = require("./config/db.js");
+
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(cors());
+
+connectDB()
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
